9bd9e9bcdfb06725ad024060d1c08addee1ecdd7
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-02-17 03:53:37 +0200

    fix structure of hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
 import { Profile } from "./Profile/Profile";
 import user from './Data/user.json'
 
+import { Stats } from "./Stats/Stats";
+import { FriendList } from "./FriendsList/FriendsList";
+import { Transactions } from "./Transactions/Transactions";
+
 export const App = () => {
   return (
-    <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
+    <div  style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}>
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  <Stats />
+  <FriendList />
+      <Transactions />
+    </div>
   );
 };
