46cc84f395f582637fd0b5f4b02b81f71f98d6a8
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-02-17 03:37:07 +0200

    mark 1st task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import { Profile } from "./Profile/Profile";
+import user from './Data/user.json'
 
 export const App = () => {
   return (
-<Profile />
+    <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
   );
 };
